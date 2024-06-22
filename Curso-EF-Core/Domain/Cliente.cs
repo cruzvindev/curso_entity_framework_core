e18fc8c4b7cf756210ce4ac6fc2c3e812bad7234
Vinicius Cruz <cruzvindev@gmail.com>
2024-06-22 15:59:26 -0300

    Utilizando data annotations para configurar o mapeamento de Cliente

--- Curso-EF-Core/Domain/Cliente.cs
+++ Curso-EF-Core/Domain/Cliente.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Curso_EF_Core.Domain;
 
+[Table("Clientes")] //Configura o nome da tabela no banco de dados
 public class Cliente
 {
+    [Key] //Configura Id como chave primária de Cliente
     public int Id { get; set; }
+
+    [Required] //O campo não pode ser nulo
     public string Nome { get; set; }
+
+    [Column("Phone")] //Configura o nome da coluna na base de dados
     public string Telefone { get; set; }
     public string CEP { get; set; }
     public string Estado { get; set; }
